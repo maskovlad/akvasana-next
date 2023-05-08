658d0e97c2fd2b4590ec3dc18d3976480d8f71b2
maskovlad <maskovladnik@gmail.com>
2023-05-08 18:10:44 +0300

    Full functionality alpha version

---
+++ pages/api/send-email.js
@@ -0,0 +1,39 @@
+const mail = require("@sendgrid/mail");
+
+mail.setApiKey(process.env.SENDGRID_API_KEY);
+const date = new Date().toLocaleString();
+
+export default (req, res) => {
+  const body = JSON.parse(req.body);
+
+  const message = `
+    <p style="font-size: 16px;">
+      Район: <b>${body.regionName}</b> <br>
+      Адреса: <b>${body.address}</b> <br>
+      Телефон: <b>${body.phone}</b> <br>
+      Клієнт?: <b>${body.isClient}</b> <br>
+      Тара: <b>${body.bottle}</b> <br>
+      Помпа: <b>${body.pomp}</b> <br>
+      Кількість: <b>${body.qty}</b> <br>
+      Сума: <b>${body.total}</b>
+    </p> 
+  `;
+
+  const data = {
+    to: "volodamir69@ukr.net",
+    from: '"Телеграм-бот 👻" <admin@sviy.site>',
+    subject: `Замовлення з Telegram ${date}`,
+    html: message,
+  };
+
+  mail
+    .send(data)
+    .then((response) => {
+      res.status(response[0].statusCode).json({ status: response[0].headers });
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+
+  
+};
