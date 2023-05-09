466b4fd1e42a6b521b5aeeb6f6148b8db8f602be
maskovlad <maskovladnik@gmail.com>
2023-05-09 18:50:36 +0300

    Added meta

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -14,15 +14,23 @@ const Layout = ({
   title: string;
   description: string;
 }) => {
+
+const router = useRouter();
+const pathname = router.pathname === "/" ? "" : router.pathname;
+
   return (
     <>
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="author" content="веб-студія Sviy.Site: www.sviy.site" />
-        <meta name="copyright" content="Аква Сана Кривий Ріг, akvasana.com.ua" />
+        <link rel="icon" href="/favicon-min.png" />
+        <meta name="author" content="Веб-студія Sviy.Site: www.sviy.site" />
+        <meta
+          name="copyright"
+          content="Аква Сана Кривий Ріг, akvasana.com.ua"
+        />
         <meta name="robots" content="index,follow" />
+        <link rel="canonical" href={`https://www.akvasana.com.ua${pathname}`} />
       </Head>
       <main className={`main ${montserrat.className}`}>{children}</main>
     </>
